0fc57f486db0b9856f9d3c0027eb06cdbf0f6fff
Jigao Luo <luojigao@outlook.com>
2017-10-26 18:44:25 +0200

    26.10 from Lenovo// the first exercise in info1

---
+++ Grundlage der Programmierung/src/Blatt1/MeinProgram.java
@@ -0,0 +1,14 @@
+package Blatt1;
+
+//@author Jigao Luo;
+
+public class MeinProgram extends MiniJava{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		write("Hello World!" + '\u2661' + "\uD83D\uDC27");
+		
+	}
+
+}
